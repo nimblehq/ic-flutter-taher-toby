4f64146d16ed547b5eba8ce8539bc50499748d28
Tobias (Toby) Heuts <tobias@nimblehq.co>
2023-03-24 10:45:05 +0700

    [#49] Fix failing UI test

--- integration_test/my_home_page_test.dart
+++ integration_test/my_home_page_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_survey/main.dart';
+import 'package:flutter_survey/ui/home/home_screen.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
@@ -9,8 +9,7 @@ void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets('My Home Page widget', (WidgetTester tester) async {
-    await tester
-        .pumpWidget(TestUtil.pumpWidgetWithShellApp(const HomeScreen()));
+    await tester.pumpWidget(TestUtil.pumpWidgetWithShellApp(HomeScreen()));
     await tester.pumpAndSettle();
 
     expect(find.widgetWithText(AppBar, 'Flutter templates testing'),
