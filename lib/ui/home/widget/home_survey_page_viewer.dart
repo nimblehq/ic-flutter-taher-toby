f01531c4cf888fa0a580a809cfc34744e6d9df77
Tobias (Toby) Heuts <tobias@nimblehq.co>
2023-03-28 15:12:14 +0700

    [#11] Fix flutter analyze

--- lib/ui/home/widget/home_survey_page_viewer.dart
+++ lib/ui/home/widget/home_survey_page_viewer.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_survey/app_navigator.dart';
 import 'package:flutter_survey/di/provider/di.dart';
-import 'package:flutter_survey/gen/assets.gen.dart';
-import 'package:flutter_survey/theme/app_dimensions.dart';
 import 'package:flutter_survey/ui/home/widget/home_survey_page.dart';
-import 'package:flutter_survey/ui/widget/dimmed_background.dart';
-import 'package:flutter_survey/ui/widget/next_button.dart';
 
 // TODO: Integrate surveys https://github.com/nimblehq/ic-flutter-taher-toby/issues/13
 class HomeSurveyPageViewer extends StatelessWidget {
