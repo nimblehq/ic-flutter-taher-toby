5fb8d80ec25a0cb3b5aebd650dc04c4fb1c0cecd
Shayokh144 <shayokhbd144@gmail.com>
2023-04-04 17:48:09 +0700

    [#7] Remove unused import

--- lib/ui/login/login_screen.dart
+++ lib/ui/login/login_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_survey/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
