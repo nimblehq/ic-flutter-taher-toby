dcbb5f0113c8943fa068fe619b7083a0f72538aa
Tobias (Toby) Heuts <tobias@nimblehq.co>
2023-04-04 11:34:48 +0700

    [#13] Integrate survey data on survey page

--- lib/api/response/survey_response.dart
+++ lib/api/response/survey_response.dart
@@ -1,3 +1,4 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'survey_response.g.dart';
@@ -6,13 +7,25 @@ part 'survey_response.g.dart';
 class SurveyResponse {
   String? id;
   String? title;
+  String? description;
+  String? coverImageUrl;
 
   SurveyResponse({
     this.id,
     this.title,
+    this.description,
+    this.coverImageUrl,
   });
 
   factory SurveyResponse.fromJson(Map<String, dynamic> json) {
     return _$SurveyResponseFromJson(json);
+  }
+
+  String getHdCoverImageUrl() {
+    if (coverImageUrl != null) {
+      return "${coverImageUrl}l";
+    } else {
+      return "";
+    }
   }
 }
