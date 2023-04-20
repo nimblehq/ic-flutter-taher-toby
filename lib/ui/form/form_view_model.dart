62bf057a8d5aee9085e102e9b21eb7e5b29f29e0
Shayokh144 <shayokhbd144@gmail.com>
2023-04-20 18:08:50 +0700

    [#10] Fix unit test error of FormViewModelTest

--- lib/ui/form/form_view_model.dart
+++ lib/ui/form/form_view_model.dart
@@ -29,7 +29,7 @@ class FormViewModel extends StateNotifier<FormState> {
       _surveyDetails.add(result.value);
       state = const FormState.loadSurveyDetailsSuccess();
     } else {
-      _error.add((result as Failed).getErrorMessage());
+      _error.add((result as Failed).getNetworkErrorMessage());
       state = const FormState.loadSurveyDetailsError();
     }
   }
