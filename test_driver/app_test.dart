a3da65c9ea50b5a78f3606975bbd3f67f9914bb2
Shayokh144 <shayokhbd144@gmail.com>
2023-04-28 14:42:06 +0700

    [#44] Add ui test for checking login success and failure scenerio

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -2,24 +2,131 @@ import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Home Screen Test', () {
+  group('Login Screen Test', () {
     late FlutterDriver driver;
+
     setUpAll(() async {
       driver = await FlutterDriver.connect();
       await driver.waitUntilFirstFrameRasterized();
     });
+
     tearDownAll(() async {
       driver.close();
     });
-    test('verify the text on login screen', () async {
-      SerializableFinder message = find.text("Email");
-      await driver.waitFor(message);
-      expect(await driver.getText(message), "Email");
+
+    test('verify the email hint text on login screen', () async {
+      SerializableFinder text = find.text('Email');
+      await driver.waitFor(text);
+      expect(await driver.getText(text), 'Email');
+    });
+
+    test('verify the password hint text on login screen', () async {
+      SerializableFinder text = find.text('Password');
+      await driver.waitFor(text);
+      expect(await driver.getText(text), 'Password');
     });
 
     test('verify the text on login button', () async {
       final buttonFinder = find.text('Log in');
       expect(await driver.getText(buttonFinder), 'Log in');
     });
+
+    test(
+      'verify the text input in textfields',
+      () async {
+        String emailValue = 'abcd@gmail.com';
+        SerializableFinder emailTextField =
+            find.byValueKey('emailTextFieldKey');
+        await driver.waitFor(emailTextField);
+        await driver.tap(emailTextField);
+        await driver.enterText(emailValue);
+        SerializableFinder emailText = find.text(emailValue);
+        await driver.waitFor(emailText);
+        expect(await driver.getText(emailText), emailValue);
+
+        String passwordValue = '12345678910';
+        SerializableFinder passwordTextField =
+            find.byValueKey('passwordTextFieldKey');
+        await driver.waitFor(passwordTextField);
+        await driver.tap(passwordTextField);
+        await driver.enterText(passwordValue);
+        SerializableFinder passwordText = find.text(passwordValue);
+        await driver.waitFor(passwordText);
+        expect(await driver.getText(passwordText), passwordValue);
+      },
+      timeout: const Timeout(
+        Duration(minutes: 2),
+      ),
+    );
+
+    test(
+      'verify login error message',
+      () async {
+        String emailValue = 'abcd@gmail.com';
+        SerializableFinder emailTextField =
+            find.byValueKey('emailTextFieldKey');
+        await driver.waitFor(emailTextField);
+        await driver.tap(emailTextField);
+        await driver.enterText(emailValue);
+        SerializableFinder emailText = find.text(emailValue);
+        await driver.waitFor(emailText);
+
+        String passwordValue = '12345678910';
+        SerializableFinder passwordTextField =
+            find.byValueKey('passwordTextFieldKey');
+        await driver.waitFor(passwordTextField);
+        await driver.tap(passwordTextField);
+        await driver.enterText(passwordValue);
+        SerializableFinder passwordText = find.text(passwordValue);
+        await driver.waitFor(passwordText);
+
+        SerializableFinder loginButton = find.byValueKey('loginButtonKey');
+        await driver.waitFor(loginButton);
+        await driver.tap(loginButton);
+
+        String errorMessage = 'Bad request';
+        SerializableFinder loginErrorText = find.text(errorMessage);
+        await driver.waitFor(loginErrorText);
+        expect(await driver.getText(loginErrorText), errorMessage);
+      },
+      timeout: const Timeout(
+        Duration(minutes: 2),
+      ),
+    );
+
+    test(
+      'verify login success and navigate to home screen',
+      () async {
+        String emailValue = 'kaung@nimblehq.co';
+        SerializableFinder emailTextField =
+            find.byValueKey('emailTextFieldKey');
+        await driver.waitFor(emailTextField);
+        await driver.tap(emailTextField);
+        await driver.enterText(emailValue);
+        SerializableFinder emailText = find.text(emailValue);
+        await driver.waitFor(emailText);
+
+        String passwordValue = '12345678';
+        SerializableFinder passwordTextField =
+            find.byValueKey('passwordTextFieldKey');
+        await driver.waitFor(passwordTextField);
+        await driver.tap(passwordTextField);
+        await driver.enterText(passwordValue);
+        SerializableFinder passwordText = find.text(passwordValue);
+        await driver.waitFor(passwordText);
+
+        SerializableFinder loginButton = find.byValueKey('loginButtonKey');
+        await driver.waitFor(loginButton);
+        await driver.tap(loginButton);
+
+        String homeScreenTextValue = 'Today';
+        SerializableFinder homeScreenText = find.text(homeScreenTextValue);
+        await driver.waitFor(homeScreenText);
+        expect(await driver.getText(homeScreenText), homeScreenTextValue);
+      },
+      timeout: const Timeout(
+        Duration(minutes: 3),
+      ),
+    );
   });
 }
