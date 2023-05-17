6276a4854a4ab02c5d3a7c9ae711067b876f18d5
Tobias (Toby) Heuts <tobias@nimblehq.co>
2023-05-17 15:30:50 +0700

    [#92] Update test for form screen

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_survey/main.dart';
+import 'package:flutter_survey/ui/form/widget/form_survey_answer_dropdown.dart';
+import 'package:flutter_survey/ui/form/widget/form_survey_answer_emoji.dart';
+import 'package:flutter_survey/ui/form/widget/form_survey_answer_multi_choice.dart';
+import 'package:flutter_survey/ui/form/widget/form_survey_answer_nps.dart';
+import 'package:flutter_survey/ui/form/widget/form_survey_answer_text_field.dart';
+import 'package:flutter_survey/ui/form/widget/form_survey_answer_textarea.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:flutter_survey/constant/widget_keys.dart';
@@ -46,6 +52,7 @@ void main() async {
     await tester.pumpAndSettle();
     await _assertLoginScreen(tester);
     await _assertHomeScreen(tester);
+    await _assertFormScreen(tester);
   });
 }
 
@@ -80,56 +87,124 @@ Future<void> _assertHomeScreen(WidgetTester tester) async {
   String secondSurveyTitleText = 'ibis Bangkok Riverside';
   expect(find.text(secondSurveyTitleText), findsOneWidget);
 
-  final nextButton = find.byKey(const Key(WidgetKeys.nextButtonKey));
-  expect(nextButton, findsOneWidget);
-  await tester.tap(nextButton);
+  final nextButtonFinder = find.byKey(const Key(WidgetKeys.nextButtonKey));
+  expect(nextButtonFinder, findsOneWidget);
+  await tester.tap(nextButtonFinder);
   await tester.pumpAndSettle();
 
   final startSurveyButtonFinder =
       find.byKey(const Key(WidgetKeys.startSurveyButtonKey));
   expect(startSurveyButtonFinder, findsOneWidget);
+  await tester.tap(startSurveyButtonFinder);
+  await tester.pumpAndSettle();
 }
 
 Future<void> _assertFormScreen(WidgetTester tester) async {
-  final nextButton = find.byKey(const Key(WidgetKeys.nextButtonKey));
-  expect(nextButton, findsOneWidget);
-  // star
-  await tester.tap(nextButton);
-  await tester.pumpAndSettle();
-  // star
-  await tester.tap(nextButton);
-  await tester.pumpAndSettle();
-  // star
-  await tester.tap(nextButton);
-  await tester.pumpAndSettle();
-  // star
-  await tester.tap(nextButton);
-  await tester.pumpAndSettle();
-  // heart
-  await tester.tap(nextButton);
-  await tester.pumpAndSettle();
-  // emoji
-  await tester.tap(nextButton);
-  await tester.pumpAndSettle();
+  final nextButtonFinder = find.byKey(const Key(WidgetKeys.nextButtonKey));
+  expect(nextButtonFinder, findsOneWidget);
+
   // multi-choice
-  await tester.tap(nextButton);
+  final multiChoiceFinder = find.byType(FormSurveyAnswerMultiChoice);
+  final multiChoiceCenter = tester.getCenter(multiChoiceFinder);
+  final multiChoiceOffset =
+      Offset(multiChoiceCenter.dx, multiChoiceCenter.dy + 100.0);
+  await tester.tapAt(multiChoiceOffset);
   await tester.pumpAndSettle();
-  // NPS
-  await tester.tap(nextButton);
-  await tester.pumpAndSettle();
-  // text-area
-  await tester.enterText(find.byType(TextField), 'Great hotel!');
-  await tester.pumpAndSettle();
-  await tester.tap(nextButton);
-  await tester.pumpAndSettle();
-  // text-fields
-  await tester.enterText(find.byType(TextField).at(0), 'My first name');
-  await tester.pumpAndSettle();
-  await tester.enterText(find.byType(TextField).at(1), 'My mobile number');
+  await tester.tap(nextButtonFinder);
   await tester.pumpAndSettle();
 
-  final submitSurveyButton =
-      find.byKey(const Key(WidgetKeys.submitSurveyButtonKey));
-  await tester.tap(submitSurveyButton);
+  // multi-choice
+  final multiChoiceFinder2 = find.byType(FormSurveyAnswerMultiChoice);
+  final multiChoiceCenter2 = tester.getCenter(multiChoiceFinder2);
+  final multiChoiceOffset2 =
+      Offset(multiChoiceCenter2.dx, multiChoiceCenter2.dy);
+  await tester.tapAt(multiChoiceOffset2);
   await tester.pumpAndSettle();
+  await tester.tap(nextButtonFinder);
+  await tester.pumpAndSettle();
+
+  // NPS
+  final npsFinder = find.byType(FormSurveyAnswerNps);
+  final npsCenter = tester.getCenter(npsFinder);
+  final npsOffset = Offset(npsCenter.dx - 100.0, npsCenter.dy);
+  await tester.tapAt(npsOffset);
+  await tester.pumpAndSettle();
+  await tester.tap(nextButtonFinder);
+  await tester.pumpAndSettle();
+
+  // star
+  final emojiAnswerFinder = find.byType(FormSurveyAnswerEmoji);
+  final emojiAnswerCenter = tester.getCenter(emojiAnswerFinder);
+  final emojiAnswerOffset =
+      Offset(emojiAnswerCenter.dx + 60.0, emojiAnswerCenter.dy);
+  await tester.tapAt(emojiAnswerOffset);
+  await tester.pumpAndSettle();
+  await tester.tap(nextButtonFinder);
+  await tester.pumpAndSettle();
+
+  // star
+  final emojiAnswerFinder2 = find.byType(FormSurveyAnswerEmoji);
+  final emojiAnswerCenter2 = tester.getCenter(emojiAnswerFinder2);
+  final emojiAnswerOffset2 =
+      Offset(emojiAnswerCenter2.dx - 60.0, emojiAnswerCenter2.dy);
+  await tester.tapAt(emojiAnswerOffset2);
+  await tester.pumpAndSettle();
+  await tester.tap(nextButtonFinder);
+  await tester.pumpAndSettle();
+
+  // star
+  final emojiAnswerFinder3 = find.byType(FormSurveyAnswerEmoji);
+  final emojiAnswerCenter3 = tester.getCenter(emojiAnswerFinder3);
+  final emojiAnswerOffset3 =
+      Offset(emojiAnswerCenter3.dx - 100.0, emojiAnswerCenter3.dy);
+  await tester.tapAt(emojiAnswerOffset3);
+  await tester.pumpAndSettle();
+  await tester.tap(nextButtonFinder);
+  await tester.pumpAndSettle();
+
+  // star
+  final emojiAnswerFinder4 = find.byType(FormSurveyAnswerEmoji);
+  final emojiAnswerCenter4 = tester.getCenter(emojiAnswerFinder4);
+  final emojiAnswerOffset4 =
+      Offset(emojiAnswerCenter4.dx + 100.0, emojiAnswerCenter4.dy);
+  await tester.tapAt(emojiAnswerOffset4);
+  await tester.pumpAndSettle();
+  await tester.tap(nextButtonFinder);
+  await tester.pumpAndSettle();
+
+  // text-area
+  final textAreaFinder = find.byType(FormSurveyAnswerTextarea);
+  await tester.enterText(textAreaFinder, 'Great hotel!');
+  await tester.pumpAndSettle();
+  await tester.tap(nextButtonFinder);
+  await tester.pumpAndSettle();
+
+  // dropdown
+  final dropdownAnswerFinder = find.byType(FormSurveyAnswerDropdown);
+  final dropdownAnswerCenter = tester.getCenter(dropdownAnswerFinder);
+  final dropdownAnswerFromOffset =
+      Offset(dropdownAnswerCenter.dx, dropdownAnswerCenter.dy);
+  final dropdownAnswerToOffset =
+      Offset(dropdownAnswerCenter.dx, dropdownAnswerCenter.dy - 200);
+  await tester.flingFrom(dropdownAnswerFromOffset, dropdownAnswerToOffset, 500);
+  await tester.pumpAndSettle();
+  await tester.tap(nextButtonFinder);
+  await tester.pumpAndSettle();
+
+  // text-fields
+  final textField1 = find.byType(TextField).at(0);
+  await tester.enterText(textField1, 'My first name');
+  await tester.pumpAndSettle();
+  final textField2 = find.byType(TextField).at(1);
+  await tester.enterText(textField2, 'My mobile number');
+  await tester.pumpAndSettle();
+
+  final submitSurveyButtonFinder =
+      find.byKey(const Key(WidgetKeys.submitSurveyButtonKey));
+  await tester.tap(submitSurveyButtonFinder);
+  await tester.pumpAndSettle();
+
+  String thankYouMessage =
+      'Thank you for taking the time to complete the survey.\nWe hope to welcome you back to ibis Bangkok Riverside soon!';
+  expect(find.text(thankYouMessage), findsOneWidget);
 }
